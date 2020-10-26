9823ef63ade93b0772d305b8ad4be3c1bcf837bf
Maksim <messiah.maks@gmail.com>
2020-10-26 20:14:09 +0300

    ready less 2

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* Created by Maksim V. Chizhov
+* IDE: qtcreator
+* Lesson 2 OOP
+*/
+
+
+#include <iostream>
+//#include "./Lesson_2/person.h"
+
+
+int main()
+{
+    std::cout << "***Task 2.1****" << std::endl;
+//Hi!!
+    std::cout << "***Task 2.2****" << std::endl;
+
+    std::cout << "***Task 2.3****" << std::endl;
+
+
+    return 0;
+}
