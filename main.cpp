8da9ed004ceba0a140304e40cfb306a8716c3a06
Maks <diablo-lod@mail.ru>
2020-10-29 19:28:26 +0300

    add main file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* Created by Maksim V. Chizhov
+* IDE: qtcreator
+* Lesson __ OOP
+*/
+
+
+#include <iostream>
+//#include "./Lesson_2/person.h"
+
+
+int main()
+{
+//    std::cout << "***Task 1.1****" << std::endl;
+
+
+    return 0;
+}
